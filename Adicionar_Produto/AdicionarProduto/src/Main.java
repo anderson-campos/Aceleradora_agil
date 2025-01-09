9abd631f03186d29e92ea9489a1532de73090b30
Anderson Campos <andersonbcamposbr@gmail.com>
2025-01-08 23:07:34 -0300

    Projeto finalizado - todas dependencias adicionadas

--- Adicionar_Produto/AdicionarProduto/src/Main.java
+++ Adicionar_Produto/AdicionarProduto/src/Main.java
@@ -3,14 +3,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("=== Sistema de Gerenciamento de Inventário ===");
 
         boolean continuar = true;
         while(continuar){
             System.out.println("\nMenu: ");
             System.out.println("1. Adicionar Produto");
-            System.out.println("2. Sair");
-            System.out.println("Escolha uma opção: ");
+            System.out.println("2. Atualizar Produto");
+            System.out.println("3. Excluir Produto");
+            System.out.println("4. Buscar Produto");
+            System.out.println("5. Filtrar por Categoria");
+            System.out.println("6. Ordenar Produtos");
+            System.out.println("7. Sair");
+            System.out.print("Escolha uma opção: ");
 
             try{
                 int opcao = Integer.parseInt(scanner.nextLine());
@@ -18,6 +24,16 @@ public class Main {
                 if (opcao == 1){
                     GerenciadorInventario.adicionarProdutos();
                 } else if (opcao == 2) {
+                    GerenciadorInventario.atualizarProduto();
+                } else if (opcao == 3) {
+                    GerenciadorInventario.excluirProduto();
+                } else if (opcao == 4) {
+                    GerenciadorInventario.buscarProduto();
+                } else if (opcao == 5) {
+                    GerenciadorInventario.filtrarPorCategoria();
+                } else if (opcao == 6) {
+                    GerenciadorInventario.ordenarProdutos();
+                } else if (opcao == 7) {
                     continuar = false;
                     System.out.println("Encerrando o sistema...");
                 } else {
--- Adicionar_Produto/AdicionarProduto/src/Produto.java
+++ Adicionar_Produto/AdicionarProduto/src/Produto.java
@@ -23,16 +23,32 @@ class Produto {
         return nome;
     }
 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
     public String getCategoria() {
         return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
     }
 
     public int getQuantidade() {
         return quantidade;
     }
 
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
     public double getPreco() {
         return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
     }
 
     @Override
